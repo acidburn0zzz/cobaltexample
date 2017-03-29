7f6a81f1e825b7dc1f782ca1646ebe9cf7302919
Jacob Walker <jacobwalker0814@gmail.com>
2017-03-29 13:44:19 -0500

    Vendor dependencies

---
+++ vendor/github.com/ardanlabs/cobalt/cobalt.go
@@ -0,0 +1,201 @@
+// Package cobalt represents a small web toolkit to allow the building of web applications.
+// It is primarily intended to be used for api web services. It allows the use of different encoders
+// such as JSON, MsgPack, XML, etc..
+//
+// Context contains the http request and response writer. It also allows parameters to be added to the context as well. Context is passed to
+// all prefilters, route handler and post filters. Context contains helper methods to extract the route parameters from the request.
+package cobalt
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type (
+	// Coder is the interface used for the encoder in Cobalt. It allows the use
+	// of multiple Encoders within cobalt
+	Coder interface {
+		Encode(w io.Writer, v interface{}) error
+		Decode(r io.Reader, v interface{}) error
+		ContentType() string
+	}
+
+	// Cobalt is the main data structure that holds all the filters, pointer to routes
+	Cobalt struct {
+		router      *httprouter.Router
+		global      []MiddleWare
+		serverError Handler
+		coder       Coder
+
+		Templates Templates
+	}
+
+	// Handler represents a request handler that is called by cobalt
+	Handler func(c *Context)
+
+	// MiddleWare is the type for middleware.
+	MiddleWare func(Handler) Handler
+)
+
+// New creates a new instance of cobalt.
+func New(coder Coder) *Cobalt {
+	return &Cobalt{router: httprouter.New(), coder: coder, Templates: DefaultTemplates()}
+}
+
+// Coder returns the Coder configured in Cobalt
+func (c *Cobalt) Coder() Coder {
+	return c.coder
+}
+
+// ServerErr sets the handler for a server err.
+func (c *Cobalt) ServerErr(h Handler) {
+	c.serverError = h
+}
+
+// NotFound sets a not found handler.
+func (c *Cobalt) NotFound(h Handler) {
+	t := func(w http.ResponseWriter, req *http.Request) {
+		ctx := NewContext(req, w, nil, c.coder, c.Templates)
+		h(ctx)
+	}
+
+	c.router.NotFound = http.HandlerFunc(t)
+}
+
+// Route adds a route with an asscoiated method, handler and route filters.. It Builds a function which is then passed to the router.
+func (c *Cobalt) route(method, route string, h Handler, m []MiddleWare) {
+
+	f := func(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+		st := time.Now()
+		ctx := NewContext(req, w, p, c.coder, c.Templates)
+
+		// Handle panics
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("cobalt: Panic, Recovering\n")
+				log.Println(r)
+				buf := make([]byte, 10000)
+				runtime.Stack(buf, false)
+				log.Printf("%s\n", string(buf))
+				if c.serverError != nil {
+					c.serverError(ctx)
+				}
+				if c.serverError == nil {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+			}
+
+			log.Printf("Request %s complete [%s] =>  %s %s - %s", ctx.ID, time.Since(st), req.Method, req.RequestURI, req.RemoteAddr)
+		}()
+
+		log.Printf("Request %s start =>  %s %s - %s", ctx.ID, req.Method, req.RequestURI, req.RemoteAddr)
+
+		w.Header().Set("X-Request-Id", ctx.ID)
+
+		mwchain := func(h Handler) Handler {
+			// global middleware.
+			for idx := range c.global {
+				h = c.global[idx](h)
+			}
+
+			// route specific middleware
+			for idx := range m {
+				h = m[idx](h)
+			}
+			return h
+		}
+
+		// process request
+		mwchain(h)(ctx)
+	}
+
+	c.router.Handle(method, route, f)
+}
+
+// Get adds a route with an associated handler that matches a GET verb in a request.
+func (c *Cobalt) Get(route string, h Handler, m ...MiddleWare) {
+	c.route("GET", route, h, m)
+}
+
+// Post adds a route with an associated handler that matches a POST verb in a request.
+func (c *Cobalt) Post(route string, h Handler, m ...MiddleWare) {
+	c.route("POST", route, h, m)
+}
+
+// Put adds a route with an associated handler that matches a PUT verb in a request.
+func (c *Cobalt) Put(route string, h Handler, m ...MiddleWare) {
+	c.route("PUT", route, h, m)
+}
+
+// Delete adds a route with an associated handler that matches a DELETE verb in a request.
+func (c *Cobalt) Delete(route string, h Handler, m ...MiddleWare) {
+	c.route("DELETE", route, h, m)
+}
+
+// Options adds a route with an associated handler that matches a OPTIONS verb in a request.
+func (c *Cobalt) Options(route string, h Handler, m ...MiddleWare) {
+	c.route("OPTIONS", route, h, m)
+}
+
+// Head adds a route with an associated handler that matches a HEAD verb in a request.
+func (c *Cobalt) Head(route string, h Handler, m ...MiddleWare) {
+	c.route("HEAD", route, h, m)
+}
+
+// ServeFiles serves files from the given file system root.
+// The path must end with "/*filepath", files are then served from the local
+// path /defined/root/dir/*filepath.
+func (c *Cobalt) ServeFiles(path string, root http.FileSystem) {
+	c.router.ServeFiles(path, root)
+}
+
+// ServeHTTP implements the HandlerFunc that process the http request.
+func (c *Cobalt) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c.router.ServeHTTP(w, req)
+}
+
+// Run runs the dispatcher which starts an http server to listen and serve.
+func (c *Cobalt) Run(addr string) {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(0)
+	log.SetPrefix("[cobalt] ")
+	log.Printf("starting, listening on %s", addr)
+
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         addr,
+		Handler:      c,
+	}
+
+	// TODO: add support for SSL/TLS
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+// RunTLS runs the dispatcher with a TLS cert.
+func (c *Cobalt) RunTLS(addr, certfile, keyfile string) {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(0)
+	log.SetPrefix("[cobalt] ")
+	log.Printf("starting, listening on %s", addr)
+
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         addr,
+		Handler:      c,
+	}
+
+	// TODO: add support for SSL/TLS
+	if err := srv.ListenAndServeTLS(certfile, keyfile); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
---
+++ vendor/github.com/ardanlabs/cobalt/context.go
@@ -0,0 +1,166 @@
+package cobalt
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/pborman/uuid"
+)
+
+const (
+	// cacheControlHeader represents the http cache control header
+	cacheControlHeader = "Cache-control"
+)
+
+type (
+
+	// Context is the struct type that holds context data for a request.
+	// Context is scoped at request level, it is currently not Go routine safe for writes, so all writes
+	// to context should be done by 1 go routine
+	Context struct {
+		ID       string
+		Response http.ResponseWriter
+		Request  *http.Request
+		Status   int
+		// data that can be stored in the context for life of request
+		data map[string]interface{}
+		// params are the request parameters from the http request
+		params    httprouter.Params
+		coder     Coder
+		templates Templates
+	}
+)
+
+// NewContext creates a new context instance with a http.Request and http.ResponseWriter.
+func NewContext(req *http.Request, resp http.ResponseWriter, p httprouter.Params, coder Coder, templates Templates) *Context {
+	return &Context{
+		ID:        uuid.New(),
+		Request:   req,
+		Response:  resp,
+		data:      make(map[string]interface{}),
+		params:    p,
+		coder:     coder,
+		templates: templates,
+	}
+}
+
+// ParamValue returns the value for the associated key from the url parameters.
+func (c *Context) ParamValue(key string) string {
+	return c.params.ByName(key)
+}
+
+// GetData returns the value for the specified key from the context data. Usually used by prefilters to pass data to the http handler
+// and post filters.
+func (c *Context) GetData(key string) interface{} {
+	data, ok := c.data[key]
+	if !ok {
+		return nil
+	}
+	return data
+}
+
+// SetData sets the data for the specified key in the context instance.
+func (c *Context) SetData(key string, value interface{}) {
+	c.data[key] = value
+}
+
+// Error returns an http Error with the specified Error string and code
+func (c *Context) Error(body interface{}, status int) {
+	c.serveEncoded(body, 0, status)
+}
+
+// Decode decodes a reader into val
+func (c *Context) Decode(r io.Reader, val interface{}) error {
+	return c.coder.Decode(r, val)
+}
+
+// DecodeBody decodes a request body into val
+func (c *Context) DecodeBody(val interface{}) error {
+	return c.coder.Decode(c.Request.Body, val)
+}
+
+// Redirect is a helper to redirect the user to a new url
+func (c *Context) Redirect(url string, status int) {
+	http.Redirect(c.Response, c.Request, url, status)
+	c.Status = status
+}
+
+// Serve is a helper method to return encoded msg based on type from a struct type.
+func (c *Context) Serve(val interface{}) {
+	c.serveEncoded(val, http.StatusOK, 0)
+}
+
+// ServeWithStatus is a helper method to return encoded msg based on type from a struct type.
+func (c *Context) ServeWithStatus(val interface{}, status int) {
+	c.serveEncoded(val, status, 0)
+}
+
+// ServeStatus serves up the status passed in.
+func (c *Context) ServeStatus(status int) {
+	if status == 0 {
+		status = http.StatusOK
+	}
+	c.Status = status
+	c.Response.WriteHeader(c.Status)
+}
+
+// ServeCachedWithStatus is a helper method to return encoded msg based on type from a struct type.
+func (c *Context) ServeCachedWithStatus(val interface{}, status int, seconds int) {
+	c.serveEncoded(val, status, seconds)
+}
+
+// serveEncoded serves a value (val) encoded with expiring in seconds and a status
+func (c *Context) serveEncoded(val interface{}, status int, seconds int) {
+	if status == 0 {
+		status = http.StatusOK
+	}
+
+	c.Response.Header().Set("Content-Type", c.coder.ContentType())
+	if seconds > 0 {
+		c.Response.Header().Set(cacheControlHeader, fmt.Sprintf("private, must-revalidate, max-age=%d", seconds))
+	}
+
+	c.Response.WriteHeader(status)
+
+	if val != nil {
+		if err := c.coder.Encode(c.Response, val); err != nil {
+			c.Response.WriteHeader(http.StatusInternalServerError)
+			status = http.StatusInternalServerError
+		}
+	}
+
+	c.Status = status
+}
+
+// ServeResponse serves a response with the status and content type sent
+func (c *Context) ServeResponse(resp []byte, status int, contentType string) {
+
+	c.Status = status
+
+	if contentType != "" {
+		c.Response.Header().Set("Content-Type", contentType)
+	}
+	if contentType == "" {
+		c.Response.Header().Set("Content-Type", c.coder.ContentType())
+	}
+	c.Response.WriteHeader(status)
+	c.Response.Write(resp)
+}
+
+// ServeHTML executes a template identified by page using the provided data and
+// serves it to the user as HTML.
+func (c *Context) ServeHTML(page string, data interface{}) {
+	var buf bytes.Buffer
+
+	if err := c.templates.Execute(&buf, page, data); err != nil {
+		log.Printf("%s error in template: %v", c.ID, err)
+		c.ServeResponse([]byte("Error in template"), http.StatusInternalServerError, "text/plain")
+		return
+	}
+
+	c.ServeResponse(buf.Bytes(), http.StatusOK, "text/html")
+}
